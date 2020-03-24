3e47c6cd493d531e05c04e094d5d1d837340d05b
nils-imhoff <github@d4d1.de>
2020-03-24 21:27:58 +0100

    fix bugs

--- cmd/src/OptParser.cpp
+++ cmd/src/OptParser.cpp
@@ -1,46 +1,38 @@
 #include "../include/OptParser.h"
-bool
-CmdLineOptParser::Parse(int argc, char* argv[])
+bool CmdLineOptParser::Parse(int argc, char* argv[])
 {
-  bool val = true;
-  int counter;
-  if (argc == 0)
-    return false;
-  if (argv == nullptr)
-    return false;
-  for (counter = 0; argv[counter] != nullptr; counter++)
-    ;
-  if (argc != counter)
-    return false;
-
-  for (int i = 1; i < argc; i++) {
-    if (argv[i][0] != '-') {
-      return false;
-    } else {
-      if (argv[i][2] == '\0') {
-        if (argc > i + 1) {
-          if (argv[i + 1][0] == '-') {
-            val &= Option(argv[i][1], nullptr);
-          } else {
-            val &= Option(argv[i][1], argv[i + 1]);
-            i++;
-          }
+    bool val = true;
+    if (argc == 0)
+        return false;
+    if (argv == nullptr)
+        return false;
+    for (int i = 1; i < argc; i++) {
+        if (argv[i][0] != '-') {
+            return false;
         } else {
-          val &= Option(argv[i][1], nullptr);
+            if (argv[i][2] == '\0') {
+                if (argc > i + 1) {
+                    if (argv[i + 1][0] == '-') {
+                        val &= Option(argv[i][1], nullptr);
+                    } else {
+                        val &= Option(argv[i][1], argv[i + 1]);
+                        i++;
+                    }
+                } else {
+                    val &= Option(argv[i][1], nullptr);
+                }
+            } else {
+                if (argv[i][2] == '=') {
+                    val &= Option(argv[i][1], &argv[i][3]);
+                } else {
+                    val &= Option(argv[i][1], &argv[i][2]);
+                }
+            }
         }
-      } else {
-        if (argv[i][2] == '=') {
-          val &= Option(argv[i][1], &argv[i][3]);
-        } else {
-          val &= Option(argv[i][1], &argv[i][2]);
-        }
-      }
     }
-  }
-  return val;
+    return val;
 }
-bool
-CmdLineOptParser::Option(const char c, const char* info)
+bool CmdLineOptParser::Option(const char c, const char* info)
 {
-  return (c == '\0' || info == nullptr);
+    return (c == '\0' && info == nullptr);
 }
--- cmd/unittest/tests.cpp
+++ cmd/unittest/tests.cpp
@@ -1,77 +1,67 @@
-/*/////////////////////////////////////////////////////////////////// //////
-* Der erste erste Test schlägt fehl da an args Leerzeichen angehängt werden*
-//////////////////////////////////////// ////////////////////////////////*/
-
 #include "../include/OptParser.h"
 #include "catch.hpp"
 
-class testOpt : public CmdLineOptParser
-{
-  bool Option(const char c, const char* info)
-  {
-    if (c == 'x') {
-      if (info == nullptr) {
-        return true;
-      }
-      int i;
-      for (i = 0; info[i]; i++)
-        ;
-      if (i > 10) {
-        return false;
-      }
+class testOpt : public CmdLineOptParser {
+    bool Option(const char c, const char* info)
+    {
+        if (c == 'x') {
+            if (info == nullptr) {
+                return true;
+            }
+            int i;
+            for (i = 0; info[i]; i++)
+                ;
+            if (i > 10) {
+                return false;
+            }
 
-      return true;
-    } else {
-      return false;
+            return true;
+        } else {
+            return false;
+        }
     }
-  }
 };
 
 TEST_CASE("Arguments parsed", "[parsing]")
 {
-  testOpt obj;
-  const char* arg0 = "programname";
-  const char* arg1 = "-x";
-  const char* arg2 = "-x=wert";
-  const char* arg3 = "-xyz";
-  const char* arg4 = "-x";
-  const char* arg5 = "wert";
-  char* args[] = { const_cast<char*>(arg0), const_cast<char*>(arg1),
-                   const_cast<char*>(arg2), const_cast<char*>(arg3),
-                   const_cast<char*>(arg4), const_cast<char*>(arg5) };
-  REQUIRE(obj.Parse(6, args) == true);
+    testOpt obj;
+    const char* arg0 = "programname";
+    const char* arg1 = "-x";
+    const char* arg2 = "-x=wert";
+    const char* arg3 = "-xyz";
+    const char* arg4 = "-x";
+    const char* arg5 = "wert";
+    char * args[] = { const_cast<char*>(arg0), const_cast<char*>(arg1),
+        const_cast<char*>(arg2), const_cast<char*>(arg3),
+        const_cast<char*>(arg4), const_cast<char*>(arg5) };
+
+    REQUIRE(obj.Parse(6, args) == true);
 }
 
 TEST_CASE("False for bad arguments", "[parsing]")
 {
-  testOpt obj;
-  SECTION("false for bad argument values")
-  {
-    const char* arg0 = "programname";
-    const char* arg1 = "-x";
-    const char* arg2 = "z";
-    const char* arg3 = "u";
-    const char* arg4 = "-x";
-    const char* arg5 = "-";
-    const char* arg6 = "-x=1234567891";
-    char* args[] = { const_cast<char*>(arg0), const_cast<char*>(arg1),
-                     const_cast<char*>(arg2), const_cast<char*>(arg3),
-                     const_cast<char*>(arg4), const_cast<char*>(arg5),
-                     const_cast<char*>(arg6) };
-    REQUIRE(obj.Parse(7, args) == false);
-  }
+    testOpt obj;
+    SECTION("false for bad argument values")
+    {
+        const char* arg0 = "programname";
+        const char* arg1 = "-x";
+        const char* arg2 = "z";
+        const char* arg3 = "u";
+        const char* arg4 = "-x";
+        const char* arg5 = "-";
+        const char* arg6 = "-x=1234567891";
+        char* args[] = { const_cast<char*>(arg0), const_cast<char*>(arg1),
+            const_cast<char*>(arg2), const_cast<char*>(arg3),
+            const_cast<char*>(arg4), const_cast<char*>(arg5),
+            const_cast<char*>(arg6) };
+        REQUIRE(obj.Parse(7, args) == false);
+    }
 
-  SECTION("false if arg is not x")
-  {
-    const char* arg0 = "programname";
-    const char* arg1 = "-y";
-    char* args[] = { const_cast<char*>(arg0), const_cast<char*>(arg1) };
-    REQUIRE(obj.Parse(2, args) == false);
-  }
-
-  SECTION("false if  argc != argv[]")
-  {
-    char* args[3];
-    REQUIRE(obj.Parse(6, args) == false);
-  }
+    SECTION("false if arg is not x")
+    {
+        const char* arg0 = "programname";
+        const char* arg1 = "-y";
+        char* args[] = { const_cast<char*>(arg0), const_cast<char*>(arg1) };
+        REQUIRE(obj.Parse(2, args) == false);
+    }
 }
