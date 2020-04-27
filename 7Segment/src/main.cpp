24b0161abf35dcdccfef1c6707f99c3686a03539
nils-imhoff <github@d4d1.de>
2020-04-27 17:40:00 +0200

    7Segment Abgabefertig

--- 7Segment/src/main.cpp
+++ 7Segment/src/main.cpp
@@ -1,8 +1,5 @@
 #include "7Segment.h"
-#include <stdio.h>
-
 int main() {
-    constexpr MultiDigit md{0,1,2,3,4,5,6,7,8,9};
-    printf("mein string: \n%s", static_cast<const char*>(md));
-    return 0;
+  constexpr MultiDigit md{0, 1, 2, 3, 4, 5, 6, 7, 8, 9};
+  return 0;
 }
